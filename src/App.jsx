53b0eeadecab381eac5249801792e06e6d1f8308
Dedei-AB <andreiabners@gmail.com>
2025-09-22 10:04:20 -0300

    arquivo env

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import Header from "./components/Header";
 import LinkGenerator from "./components/LinkGenerator";
 import ContactBook from "./components/ContactBook";
-import { createClient } from "@supabase/supabase-js";
 import { useState } from "react";
+import { supabase } from "./supabaseCLient";
 import "./App.css";
 
 const App = () => {
-  const [tarefas, setTarefas] = useState([]);
-  const URL = "https://qmvnxspxwipsdmblydmg.supabase.co";
-  const KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtdm54c3B4d2lwc2RtYmx5ZG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1Mzk4NjgsImV4cCI6MjA3NDExNTg2OH0.wCWJbb2F64MhoKg7vvKZ_s2aa-EKPVmyjVKzSyHntu4";
-  const supabase = createClient(URL, KEY);
-
   const consultaTarefas = async () => {
     const [data, error] = await supabase.from("tarefas").select("*");
     setTarefas(data);
---
+++ src/supabaseCLient.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const [tarefas, setTarefas] = useState([]);
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+export const supabase = createClient(URL, KEY);
