7bca56d21492c920b0fcd4d982d2ad60043b4203
Dedei-AB <andreiabners@gmail.com>
2025-09-26 09:28:30 -0300

    Link copiado

--- src/components/CopyLink.jsx
+++ src/components/CopyLink.jsx
@@ -1,11 +1,21 @@
-import { MessageCircle, Clipboard } from "lucide-react";
+import { MessageCircle, Clipboard, FastForward } from "lucide-react";
+import { useState } from "react";
 import styles from "./CopyLink.module.css";
 
 function CopyLink({ linkDois }) {
+  const [linkCopiado, setLinkCopiado] = useState(false);
+
+  const copiarLink = () => {
+    navigator.clipboard.writeText(linkDois);
+    setLinkCopiado(true);
+    setTimeout(() => {
+      console.log("Link copiado");
+      setLinkCopiado(false);
+    }, 1000);
+  };
   return (
     <div className={styles["link-container"]}>
       <label className={styles["formLabel"]}>Link gerado:</label>
-
       <div className={styles["link-box"]}>
         <input
           type="text"
@@ -13,10 +23,10 @@ function CopyLink({ linkDois }) {
           readOnly
           className={styles["link-input"]}
         />
-        <button
-          className={styles["copy-button"]}
-          onClick={() => navigator.clipboard.writeText(linkDois)}
-        >
+        <button className={styles["copy-button"]} onClick={copiarLink}>
+          {linkCopiado && (
+            <p className={styles["link-copiado"]}>Link copiado!</p>
+          )}
           <Clipboard />
         </button>
       </div>
